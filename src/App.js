c5032a7524add74c68bbf6f6347fbcad7646af9d
maung-htay <ukyishwejp@gmail.com>
2024-04-28 22:58:28 +0900

    Checking auto deploy of github action

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React + CI CD test
+          Learn React + CI CD test + Next Update
         </a>
       </header>
     </div>
